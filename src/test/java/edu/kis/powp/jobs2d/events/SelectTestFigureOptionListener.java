00a5f447b3fa216837c6a5a7a228300a2b6e50a2
justkinou <justkinou@proton.me>
2026-03-12 14:33:53 +0100

    add action for figureScript2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,12 +3,17 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
+	@FunctionalInterface
+	private interface FigureScript {
+		void figureScript(Job2dDriver driver);
+	}
 
-	private DriverManager driverManager;
+	private final DriverManager driverManager;
 
 	public SelectTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
@@ -16,6 +21,11 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		FigureScript figureScript = switch (e.getActionCommand()) {
+			case "Figure Joe 1" -> FiguresJoe::figureScript1;
+			case "Figure Joe 2" -> FiguresJoe::figureScript2;
+            default -> throw new IllegalStateException("Unexpected value: " + e.getActionCommand());
+        };
+		figureScript.figureScript(driverManager.getCurrentDriver());
 	}
 }
