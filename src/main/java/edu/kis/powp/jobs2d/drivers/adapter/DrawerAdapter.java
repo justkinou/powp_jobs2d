cda125743438cc4bd8c70ba1132cf2e169599c4d
justkinou <justkinou@proton.me>
2026-03-12 15:16:38 +0100

    replace manual position change to setPosition call

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -26,8 +26,7 @@ public class DrawerAdapter implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		this.startX = x;
-		this.startY = y;
+		setPosition(x, y);
 
 		DrawerFeature.getDrawerController().drawLine(line);
 	}
